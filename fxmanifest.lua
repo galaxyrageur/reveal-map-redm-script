2c549fc12dcae1946b60e0b7fdffd927d82f471f
galaxyrageur <96849782+galaxyrageur@users.noreply.github.com>
2026-07-30 20:47:22 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-author 'toi'
+author 'galaxyrageur'
 description 'Révèle toute la carte au joueur à sa connexion'
 version '1.0.0'
 
